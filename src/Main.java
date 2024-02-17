3b61e4a14b42fcf26bf6463be581b8d42b8c8891
Miguel <miguel.eenobrega@gmail.com>
2024-02-17 20:11:32 +0100

    (feat) Main hecho completo

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,115 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
+import Cine;
+import Pelicula;
+import Sala;
+
 public class Main {
+
     public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Scanner entrada = new Scanner(System.in);
+        boolean seguir = true;
+
+        Pelicula pelicula1 = new Pelicula("Oppenheimer",180);
+        Pelicula pelicula2 = new Pelicula("Fight Club",139);
+        Pelicula pelicula3 = new Pelicula("Shutter Island",138);
+        Pelicula pelicula4 = new Pelicula("The Departed",151);
 
 
+        Sala sala1 = new Sala(1, pelicula1, 8, 8);
+        Sala sala2 = new Sala(2, pelicula2, 6, 8);
+        Sala sala3 = new Sala(3, pelicula3, 8, 10);
+        Sala sala4 = new Sala(4, pelicula4, 5, 10);
+        Sala sala5 = new Sala(5, pelicula1, 5, 8);
+        Sala sala6 = new Sala(6, pelicula4, 4, 6);
+
+
+        Cine cine = new Cine("CINESA, Manoteras", 306, new Sala[]{sala1,sala2,sala3,sala4,sala5,sala6});
+
+        /*--------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+        do {
+
+            System.out.println("\nSeleccione una opción: ");
+
+            System.out.println("1. Ver Salas y la película. "
+                    + "\n2. Ver Asientos de las Salas. "
+                    + "\n3. Comprar Entrada. "
+                    + "\n4. Salir.");
+
+            int op = entrada.nextInt();
+
+            switch(op) {
+
+
+                case 1:
+                    cine.verSalas();
+
+                    break;
+
+
+
+                case 2:
+
+                    System.out.println("Número de Sala: ");
+                    int nSala = entrada.nextInt();
+                    cine.mostrarPlanoAsientos(nSala);
+
+                    break;
+
+
+                case 3:
+
+                    System.out.println("Número de Sala: ");
+                    int nSalaC = entrada.nextInt();
+
+                    if(nSalaC<1 || nSalaC>cine.getSalas().length) {
+                        System.out.println("Sala No encontrada");
+
+                    }else {
+
+                        cine.mostrarPlanoAsientos(nSalaC);
+                        System.out.println("Ingresar correo electrónico: ");
+                        String correoC=entrada.next();
+                        System.out.println("Ingresa Fila y Columna de tu butaca: ");
+                        int filaC=entrada.nextInt();
+                        int columnaC=entrada.nextInt();
+
+                        cine.comprarEntrada(nSalaC, filaC, columnaC, correoC);
+                    }
+
+                    break;
+
+
+                case 4:
+
+                    System.out.println("Gracias por su visita. Vuelva Pronto!");
+                    seguir=false;
+
+                    break;
+
+
+                default:
+
+                    System.out.println("Error");
+                    seguir=false;
+
+                    break;
+
+
+            }
+
+        }while(seguir);
+
+
+        entrada.close();
 
 
     }
+
 }
