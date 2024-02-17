3023f84400106636065b4e1c8a7d8f4b68660590
Miguel <miguel.eenobrega@gmail.com>
2024-02-17 18:54:58 +0100

    (feat) Atributos, Constructor, getters y setters añadidos para el objeto Pelicula

---
+++ src/Pelicula.java
@@ -0,0 +1,50 @@
+public class Pelicula {
+
+// Campos de la clase:
+
+    private String titulo;
+    private int duracion;
+
+
+    public Pelicula(String titulo, int duracion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+    }
+
+
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
